309e4889572aa4211aace580c7e2938a987c4520
Bartosz <bartoszchmielecki2003@gmail.com>
2025-03-26 00:06:28 +0100

    Usine Next.js Links

--- e-commerce/app/ProductsList.tsx
+++ e-commerce/app/ProductsList.tsx
@@ -1,15 +1,21 @@
-import { Product} from './product-data'
-import Image from 'next/image';
-export default function ProductsList({products}: { products: Product[]}) {
+import { Product } from "./product-data";
+import Link from "next/link";
+import Image from "next/image";
+export default function ProductsList({ products }: { products: Product[] }) {
   return (
     <div>
-      {products.map(product => (
-        <div key={product.id}>
-          <Image src={`/`+product.imageUrl} alt={product.name} width={150} height={150}/>
+      {products.map((product) => (
+        <Link key={product.id} href="/product-detail">
+          <Image
+            src={`/` + product.imageUrl}
+            alt={product.name}
+            width={150}
+            height={150}
+          />
           <h2>{product.name}</h2>
           <p>${product.price}</p>
-        </div>
+        </Link>
       ))}
     </div>
-  )
+  );
 }
--- e-commerce/app/product-detail/page.tsx
+++ e-commerce/app/product-detail/page.tsx
@@ -1,3 +1,3 @@
 export default function ProductsDetailPage() {
-  return <h1>Products Dssselllltails</h1>;
+  return <h1>Products Details</h1>;
 }
