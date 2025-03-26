c80d3262239dce3f191dc7ca6976ec9971dc2d34
Bartosz <bartoszchmielecki2003@gmail.com>
2025-03-26 23:02:51 +0100

    1

--- e-commerce/app/page.tsx
+++ e-commerce/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/product-detail"
+            href="/products"
             target="_blank"
             rel="noopener noreferrer"
           >
